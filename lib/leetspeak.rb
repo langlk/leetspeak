804cb2ab02936bf3a2552e920900122ed8dd7e4f
kelsey langlois and LDMacKrell <kelsey@>
2017-09-05 11:25:24 -0700

    Update leetspeak to replace os in message with 0s

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,5 +1,7 @@
 class String
   def leetspeak()
+    self.gsub!("o", "0")
+    self.gsub!("O", "0")
     self.gsub!("e", "3")
     self.gsub!("E", "3")
     self
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -9,4 +9,8 @@ describe('#leetspeak') do
   it("replaces every 'e' or 'E' in a string with a '3'") do
     expect(("keep").leetspeak()).to(eq("k33p"))
   end
+
+  it("replaces every 'o' or 'O' in a string with a '0'") do
+    expect(("boo boo").leetspeak()).to(eq("b00 b00"))
+  end
 end
