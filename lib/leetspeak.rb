ee0c2fc389323c1ffd2efb1d1558a5d6b40e501e
kelsey langlois and LDMacKrell <kelsey@>
2017-09-05 11:23:07 -0700

    Update leetspeak to replace es in string with 3s.

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,5 +1,7 @@
 class String
   def leetspeak()
+    self.gsub!("e", "3")
+    self.gsub!("E", "3")
     self
   end
 end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -5,4 +5,8 @@ describe('#leetspeak') do
   it("returns a string as is when no leetspeak rules apply") do
     expect(("jumpy").leetspeak()).to(eq("jumpy"))
   end
+
+  it("replaces every 'e' or 'E' in a string with a '3'") do
+    expect(("keep").leetspeak()).to(eq("k33p"))
+  end
 end
