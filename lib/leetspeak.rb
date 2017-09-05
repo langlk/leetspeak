b00098aea96ce2d7bdccc323673ad0f318ed16e9
kelsey langlois and LDMacKrell <ldmackrell@>
2017-09-05 11:28:34 -0700

    Update leetspeak to replace ts in string with 7s

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,5 +1,8 @@
 class String
   def leetspeak()
+    self.gsub!("t", "7")
+    self.gsub!("T", "7")
+    self.gsub!("I", "1")
     self.gsub!("o", "0")
     self.gsub!("O", "0")
     self.gsub!("e", "3")
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -13,4 +13,12 @@ describe('#leetspeak') do
   it("replaces every 'o' or 'O' in a string with a '0'") do
     expect(("boo boo").leetspeak()).to(eq("b00 b00"))
   end
+
+  it("replaces every 'I' in a string with a '1'") do
+    expect(("I like Ike").leetspeak()).to(eq("1 lik3 1k3"))
+  end
+
+  it("replaces every 't' or 'T' in a string with a '7'") do
+    expect(("teeth").leetspeak()).to(eq("7337h"))
+  end
 end
